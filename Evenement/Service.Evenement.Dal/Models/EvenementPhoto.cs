d443f9ffe1ff9f95eb881f07a46024dfcf54965a
LucasLibis <lucas.libis@hotmail.fr>
2014-10-24 17:41:57 +0200

    #3 Création des modèles - EvenementPhoto

---
+++ Evenement/Service.Evenement.Dal/Models/EvenementPhoto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Evenement.Dal.Models
+{
+    public class EvenementPhoto
+    {
+        public long EvenementPhoto_id { get; set; }
+        public long Evenement_id { get; set; }
+        public string Adresse { get; set; }
+    }
+}
