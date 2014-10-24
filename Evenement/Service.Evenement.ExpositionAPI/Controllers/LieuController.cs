8c74384681a647ad6afac4ba07b3ffee9f27577e
jbouchareine <julien.bouchareine@y-nov.com>
2014-10-24 16:58:22 +0200

    #7 mapping lieux

---
+++ Evenement/Service.Evenement.ExpositionAPI/Controllers/LieuController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Service.Evenement.ExpositionAPI.Controllers
+{
+    public class LieuController : ApiController
+    {
+        // GET api/lieu
+        public IEnumerable<object> GetAll()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/lieu/5
+        public string GetIById(int id)
+        {
+            return "value";
+        }
+    }
+}
