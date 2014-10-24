824cf7a4fa57232fc46b5f3da476d41d7deac575
LucasLibis <lucas.libis@hotmail.fr>
2014-10-24 18:47:04 +0200

    #6 Création des modèles - Catégorie

---
+++ Evenement/Service.Evenement.Dal/Models/Categorie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Evenement.Dal.Models
+{
+    public class Categorie
+    {
+        public long Categorie_id { get; set; }
+        public string Libelle { get; set; }
+    }
+}
