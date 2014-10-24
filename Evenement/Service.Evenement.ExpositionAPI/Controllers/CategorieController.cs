53377d2dbf85e9d241bb802386bdf5f1b09394f7
briceJantieu <brice.jantieu@gmail.com>
2014-10-24 16:31:11 +0200

    [F02] [DSF03]#1 création des routes pour la catégorie

---
+++ Evenement/Service.Evenement.ExpositionAPI/Controllers/CategorieController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Service.Evenement.ExpositionAPI.Controllers
+{
+    public class CategorieController : ApiController
+    {
+        // GET api/Categorie
+        public IEnumerable<object> GetAll()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/Categorie/5
+        public string GetById(int id)
+        {
+            return "value";
+        }
+
+    }
+}
