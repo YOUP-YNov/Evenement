b775bf1c4e764b38c8f53714ea0d0f94aa74acc8
MVPSoon <bozzi.loic@gmail.com>
2014-10-26 14:02:25 +0100

    Revert "Revert "[NOTAG] [DAL] Création DAO/Structure csProj Init ~ Création DAO""
    This reverts commit 0d3df836cf471b261d98638510fba28fd1d04b75.
    
    Conflicts:
    	Evenement/Service.Evenement.Dal/EvenementDalService.cs
    	Evenement/Service.Evenement.Dal/Service.Evenement.Dal.csproj

---
+++ Evenement/Service.Evenement.Dal/Dao/Evenement.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Evenement.Dal.Dao
+{
+    public class Evenement
+    {
+        /// <summary>
+        /// Assigne ou récupère l'id de l'évenement.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère l'id de l'organisateur de l'évenement.
+        /// </summary>
+        public int OrganisateurId { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère la liste des HashTag associé à l'évenement
+        /// </summary>
+        public IEnumerable<StringBuilder> HashTag { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère la liste des Images associé à l'évenement
+        /// </summary>
+        public IEnumerable<EventImage> Galleries { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère l'adresse de l'évenement
+        /// </summary>
+        public EventLocation EventAdresse { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère le libelle de la catégorie de l'évenement
+        /// </summary>
+        public StringBuilder Categorie { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère l'id de la catégorie de l'évenement
+        /// </summary>
+        public int CategorieId { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère la Date à laquelle l'évenement a lieu.
+        /// </summary>
+        public DateTime DateEvenement { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère la Date à laquelle l'évenement a lieu.
+        /// </summary>
+        public DateTime CreateDate { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère la Date à laquelle l'évenement a était crée.
+        /// </summary>
+        public DateTime DateModification { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère la Date à partir de laquelle les utilisateurs ne peuvent plus
+        /// s'incrirent.
+        /// </summary>
+        public DateTime DateFinInscription { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère le titre de l'évenement
+        /// </summary>
+        public StringBuilder TitreEvenement { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère la description de l'évenement
+        /// </summary>
+        public StringBuilder DescriptionEvenement { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère le nombre de participant maximum à l'évenement
+        /// </summary>
+        public int MaximumParticipant { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère le nombre de participant minimum à l'évenement
+        /// </summary>
+        public int MinimumParticipant { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère l'état de l'évenement
+        /// </summary>
+        public EventState EtatEvenement { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère le price a payé pour participé à l'évenement
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère l'attestation que l'évenement est premium
+        /// </summary>
+        public bool Premium { get; set; }
+
+        /// <summary>
+        /// Assigne ou récupère la date de mise en avant de l'évenement
+        /// </summary>
+        public DateTime DateMiseEnAvant { get; set; }
+    }
+}
---
+++ Evenement/Service.Evenement.Dal/Interface/IEvenementDalService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Evenement.Dal.Interface
+{
+    interface IEvenementDalService : IDisposable
+    {
+
+    }
+}
