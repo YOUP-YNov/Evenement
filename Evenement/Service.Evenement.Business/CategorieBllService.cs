da6349c856cf289a0283dbcbe1b0f6e2abf1f6e3
briceJantieu <brice.jantieu@gmail.com>
2014-10-31 15:31:12 +0100

    method getCategorie part1

--- Evenement/Service.Evenement.Business/CategorieBllService.cs
+++ Evenement/Service.Evenement.Business/CategorieBllService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Service.Evenement.Dal;
+using Service.Evenement.Dal.Dao.Request;
 
 namespace Service.Evenement.Business
 {
@@ -25,8 +26,25 @@ namespace Service.Evenement.Business
             }
         }
 
-        public EvenementCategorieBll GetCategories ()
+        /*public EvenementCategorieBll GetCategories ()
         {
+            var result = _evenementDalService.GetAllCategorie;
+            return result; 
         }
+
+        public EvenementCategorieBll GetCategorie(long id)
+        {
+             Dal.Dao.EvenementCategorieDao categ = new  Dal.Dao.EvenementCategorieDao();
+            categ.Id = id;
+            IEnumerable<Dal.Dao.EvenementCategorieDao> result = _evenementDalService.GetAllCategorie(new EvenementDalRequest(){ Categorie = categ});
+            return ((result = null) ? null : result.First);
+
+        }
+
+        public void ExampleAutoMapper(EvenementDao daoEvent)
+        {
+            Mapper.CreateMap<EvenementDao, EvenementBll>();
+            EvenementBll bllEvent = Mapper.Map<EvenementDao, EvenementBll>(daoEvent);
+        }*/
     }
 }
