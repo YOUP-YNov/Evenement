01d8e0f553972a732e56f432da2e79f0f8556e28
Ludovic <ludovic.panchaud@gmail.com>
2014-10-31 18:32:47 +0100

    [F01][DSF01] Lister les événements #TestDAL

---
+++ Evenement/Service.Evenement.Front.Tests/UnitTestEvenement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.Collections.Generic;
+using Service.Evenement.Dal;
+
+
+namespace Service.Evenement.Front.Tests
+{
+    [TestClass]
+    public class UnitTestEvenement
+    {
+        [TestMethod]
+        public void TestGetEvenements ()
+        {
+            EvenementDalService test = new EvenementDalService();
+
+            List<Evenement.Dal.Dao.EvenementDao> list = new List<Dal.Dao.EvenementDao>(test.GetAllEvenement());
+
+            Assert.AreEqual(101, list.Count);
+        }
+    }
+}
