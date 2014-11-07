5298478f0da21dcbccfb6bbc34548f525253f915
DimitriHeloin <dimitri.heloin@ingesup.com>
2014-11-07 15:50:16 +0100

    [F02] [DSF05] Supprimer une catégorie

--- Evenement/Service.Evenement.Business/BusinessModels/EventImage.cs
+++ Evenement/Service.Evenement.Business/BusinessModels/EventImage.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,26 +19,3 @@ namespace Service.Evenement.Business
         public long Id { get; set; }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Service.Evenement.Business
-{
-    public class EventImageBll
-    {
-        /// <summary>
-        /// Assigne ou récupère l'url de l'image
-        /// </summary>
-        public StringBuilder Url { get; set; }
-
-        /// <summary>
-        /// Assigne ou récupère l'id de l'image
-        /// </summary>
-        public long Id { get; set; }
-    }
-}
->>>>>>> e09567d32f347f4bd712ea940432e8e81154da95
